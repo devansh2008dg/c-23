f1b97b2ce0283c379408583d7c46a36b93d07ce7
devansh2008dg <74973237+devansh2008dg@users.noreply.github.com>
2021-01-06 18:47:23 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+class Box{
+    constructor(x,y,w,h){
+        var options={
+            restitution:1
+        };
+        this.body=Bodies.rectangle(x,y,w,h,options);
+        this.width=w;
+        this.height=h;
+        World.add(world,this.body);
+
+    }
+    display(){
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+
+        strokeWeight(4);
+        stroke("yellow");
+        fill(0);
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+}
